1bfd393903e4b62590f4b1b40e9fcc731cf37afa
yd <pokar.yashusmile@gmail.com>
2021-12-12 15:39:17 +0530

    initialcommit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
